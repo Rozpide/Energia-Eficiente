a4a53bcd04dd9ac0dd74667bb9f42c291fb1b4eb
Pablo Querales <119375308+PabloQuerales@users.noreply.github.com>
2025-02-27 23:48:32 +0000

    base de datos creada

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,71 @@
+from typing import List
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String, create_engine, ForeignKey
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.email}>'
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(80),nullable=False)
+    first_name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    last_name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    birthdate: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    country: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    accounts: Mapped[List["Accounts"]] = relationship()
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "birthdate": self.birthdate,
+            "country": self.country,
             # do not serialize the password, its a security breach
         }
+
+class Accounts(db.Model):
+    __tablename__ = 'accounts'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    name: Mapped[str] = mapped_column(String(80),nullable=False)
+    balance: Mapped[int] = mapped_column(unique=True, nullable=False)
+    coin: Mapped[int] = mapped_column(unique=True, nullable=False)
+    type: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    accounts: Mapped[List["Account_details"]] = relationship()
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "name": self.name,
+            "balance": self.balance,
+            "coin": self.coin,
+            "type": self.type
+        }
+
+class Account_details(db.Model):
+    __tablename__ = 'account_details'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    accounts_id: Mapped[int] = mapped_column(ForeignKey("accounts.id"))
+    detail: Mapped[str] = mapped_column(String(80),nullable=False)
+    amount: Mapped[int] = mapped_column(unique=True, nullable=False)
+    coin: Mapped[int] = mapped_column(String(120), unique=True, nullable=False)
+    type: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    date: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    time: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "accounts_id": self.accounts_id,
+            "detail": self.detail,
+            "amount": self.amount,
+            "coin": self.coin,
+            "type": self.type,
+            "date": self.date,
+            "time": self.time,
+        }   
