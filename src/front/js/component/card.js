a627d1bae1a227670ee2a6c227d3b6f5942e3572
Jennifer Fernández <j.fdezmurga@gmail.com>
2025-03-01 11:40:50 +0000

    second commit home

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -2,20 +2,20 @@ import React, { Component } from "react";
 
 export const Card = () => {
     return (
-<div className="card m-2" style={{width:"18rem"}}>
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
+<div className="card m-2" style={{ width: "18rem" }}>
+  <img 
+    src="https://static.zoomalia.com/prod_img/46527/la_53533e8075e9970de0cfea955afd4644bb21537446021.jpg" 
+    style={{ width: "150px", height: "200px" }} 
+    className="card-img-top mx-auto d-block mt-2" 
+    alt="..." 
+  />
+  <div className="card-body text-center">
+    <h5 className="card-title">Título</h5>
     <p className="card-text">Some quick example text to build on the card.</p>
   </div>
-  <ul className="list-group list-group-flush">
-    <li className="list-group-item">An item</li>
-    <li className="list-group-item">A second item</li>
-  </ul>
-  {/* <div className="card-body">
-    <a href="#" className="card-link">Card link</a>
-    <a href="#" className="card-link">Another link</a>
-  </div> */}
+  <div className="mt-auto text-center">
+     <button className="btn btn-primary m-2">Añadir carrito</button>
+  </div>
 </div>
 );
 }
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,7 +6,7 @@ import { CiSearch } from "react-icons/ci";
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Pupper Eats</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
