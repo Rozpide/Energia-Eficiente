23c31959634abbfcf1d656ebc480398e7f86dd26
Camilo Cortes <100862481+camilocortes27@users.noreply.github.com>
2025-03-01 17:34:10 +0000

    creacion card

--- src/front/js/pages/principalPage.js
+++ src/front/js/pages/principalPage.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { Sidebar } from "../component/sidebar";
+import { Card } from "../component/card";
 
 export const PrincipalPage = () => {
     return (
-        <Sidebar />
+        <>
+            <div className="d-flex">
+                <Sidebar />
+                <div>
+                    <Card/>
+                </div>
+            </div>
+        </>
     )
 }
