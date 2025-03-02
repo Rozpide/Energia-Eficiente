0cf26d8c312b955667a150bb69832e479680c1ee
Luis Malizia <luisfmaliziam@gmail.com>
2025-03-02 11:26:31 +0000

    se crearon los endpoinst para mostrar todas la cuentas y una cuenta especifica de un usuario especifico

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,10 +8,8 @@ from flask_cors import CORS
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required,verify_jwt_in_request
 from flask_jwt_extended.exceptions import NoAuthorizationError
 
-
 api = Blueprint('api', __name__)
 CORS(api)
-
 # @api.route('/hello', methods=['POST', 'GET'])
 # def handle_hello():
 
@@ -20,21 +18,18 @@ CORS(api)
 #     }
 
 #     return jsonify(response_body), 200
-
+        #este endpoint busca y muestra a todos los usuarios registrados
 @api.route('/users', methods=['GET'])
 def get_users():
-
     data = db.session.scalars(db.select(User)).all()
     result = list(map(lambda item: item.serialize(),data))
-    
     if result == []:
         return jsonify({"msg":"Usuario no encontrado"}), 404
-    
     response_body = {
         "results": result
     }
     return jsonify(response_body), 200
-
+        #este endpoint busca un usuario especifico entre todos los demas
 @api.route('/user/<int:user_id>', methods=['GET'])
 def get_one_user(user_id):
     try:
@@ -42,10 +37,9 @@ def get_one_user(user_id):
         return jsonify({"result":user.serialize()}), 200
     except:
         return jsonify({"msg":"Usuario o contraseña incorrecta"}), 404
-    
+    #este endpoint valida los datos de usuario y crea el token de acceso
 @api.route("/login", methods=["POST"])
 def login():
-
     email = request.json.get("email", None)
     password = request.json.get("password", None)
     try:
@@ -56,55 +50,60 @@ def login():
         return jsonify(access_token=access_token)
     except:
         return jsonify({"msg": "this user does not exist"}), 404
-    
+        #este endpoin protege la ruta del usuario
 @api.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
-    
+        #este endpoint crea una cuenta con el  id del usuario
 @api.route('/<int:user_id>/new-account', methods=['POST'])
 def post_account(user_id):
     try:
         request_body = request.json
 
         exist = db.session.query(db.select(Accounts).filter_by(name=request_body["name"]).exists()).scalar()
-        print(exist)
         if not exist: 
-            print(exist)
-            print(request_body)
             new_account = Accounts(user_id=user_id, name=request_body["name"], balance=request_body["balance"], coin=request_body["coin"], type=request_body["type"])
             db.session.add(new_account)
             db.session.commit()  
             return jsonify(request_body), 200
-
         else:
             return jsonify({"msg": "Account already exist"}), 404
     except Exception as e:
         return jsonify({"msg":"Error", "error": str(e)}), 500
-        
-
+        #este endpint muestra todas cuentas en general
 @api.route('/accounts', methods=['GET'])
 def get_accounts():
-
     data = db.session.scalars(db.select(Accounts)).all()
     result = list(map(lambda item: item.serialize(),data))
-    
     if result == []:
         return jsonify({"msg":"no accounts, please create one"}), 404
-
     response_body = {
         "results": result
     }
-
     return jsonify(response_body), 200
-
-@api.route("/verify-token", methods=["GET"])
-def verify_token():
+        # este endpoin busca la lista de cuentas y muestra una sola cuenta especifica
+@api.route('/accounts/<int:accounts_id>', methods=['GET'])
+def get_one_accounts(accounts_id):
     try:
-        verify_jwt_in_request()  # Verifica la validez del token
-        identity = get_jwt_identity()  # Obtiene el usuario del token
-        return jsonify({"valid": True, "user": identity}), 200
-    except NoAuthorizationError:
-        return jsonify({"valid": False, "message": "Token inválido o no proporcionado"}), 401
+        account = db.session.execute(db.select(Accounts).filter_by(id=accounts_id)).scalar_one()
+        return jsonify({"result":account.serialize()}), 200
+    except:
+        return jsonify({"msg":"account not found"}), 404
+         #este endpoint valida si existe el usuario y muestra las cuentas de un usuario especifico
+@api.route('/user/<int:user_id>/accounts', methods=['GET'])
+def get_one_account_to_one_user(user_id):
+    try:
+        exist = db.session.query(db.select(Accounts).filter_by(user_id=user_id).exists()).scalar()
+        if exist:
+            accounts = db.session.execute(db.select(Accounts).filter_by(user_id=user_id)).scalars().all()
+            if accounts != []:
+                return jsonify({"result": [acc.serialize() for acc in accounts]}), 200
+            return jsonify({"msg": "No accounts to show"})
+        else:
+            return jsonify({"msg": "user doesn't exist"}), 404
+        
+    except Exception as e:
+        return jsonify({"msg":"Error", "error": str(e)}), 500
