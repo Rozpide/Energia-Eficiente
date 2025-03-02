7d597f8342ec76d17a0d6a6ff8fe97f6437b303f
Jesus F Plaz <jfplaz@gmail.com>
2025-03-02 11:50:08 +0000

    navbar

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
+import { Loginview } from "./pages/loginview";
+import { ForgoPassword } from "./pages/resetpassword";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
@@ -26,6 +28,8 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
+                        <Route element={<Loginview />} path="/login" />
+                        <Route element={<ForgoPassword />} path="/resetpassword" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
---
+++ src/front/js/pages/loginview.js
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+import { Link } from "react-router-dom";
+
+export const Loginview = () => {
+  // const { store, actions } = useContext(Context);
+
+  return (
+
+
+    <div className="container-fluid mx-auto d-flex marginlogintop justify-content-center">
+      <div className="card border border-0 marginloginright" style={{ width: "18rem" }}>
+        <img src="https://i.postimg.cc/pd5s9QTf/Sin-t-tulo.jpg" alt="..." />
+
+
+      </div>
+
+      <form>
+        <div className="mb-3">
+          <div className="d-flex justify-content-center mb-4 fs-1 textcolors">Welcome Back!</div>
+          <div className="d-flex justify-content-center mb-4">Login below or&nbsp;<a href="forgot password"> create an account</a>
+          </div>
+          <label for="exampleInputEmail1" className="form-label ">Email address</label>
+          <input type="email" className="form-control bordercolor border border-3" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control bordercolor border border-3" id="exampleInputPassword1" />
+        </div>
+        <button type="submit" className="backbutton  w-100 border border rounded p-1 mb-1">Sign in</button>
+        <Link to={"/resetpassword/"} className="d-flex justify-content-center mt-2">
+          Have you forgotten your password?
+        </Link>
+      </form >
+    </div >
+  );
+};
