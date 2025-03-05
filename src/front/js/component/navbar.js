ef40be1fb72b9e901101e0c32d6be7c6d2efc0bf
Jesus F Plaz <jfplaz@gmail.com>
2025-03-05 09:36:13 +0000

    Thememode1

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Thememode } from "./Thememode"
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light p-0">
 			<div className="container-fluid margintop">
-			<Link to="/">
-                    <img
-                        src="https://i.postimg.cc/76MM7Ky2/Sin-t-tulo2.jpg"
-                        className="navbar-brand backnavbar	"
-                        style={{ width: "3rem" }}
-                    />
-                </Link>
+				<Link to="/">
+					<img
+						src="https://i.postimg.cc/76MM7Ky2/Sin-t-tulo2.jpg"
+						className="navbar-brand backnavbar	"
+						style={{ width: "3rem" }}
+					/>
+				</Link>
 				<div className="ml-">
 					<Link to="/login">
 						<button className=" w-100 me-5 backbutton border border rounded text-black p-1">Login</button>
 					</Link>
 				</div>
 			</div>
+			<Thememode/>
 		</nav>
 	);
 };
