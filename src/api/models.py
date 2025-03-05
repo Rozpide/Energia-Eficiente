2a1e870c9707a198f9ba189b98183dedb80aeee9
Rafael Salinas Gonzalez <rafsalgon@gmail.com>
2025-03-05 20:00:28 +0000

    Database models Second commit

--- src/api/models.py
+++ src/api/models.py
@@ -2,86 +2,103 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     password = db.Column(db.String(80), unique=False, nullable=False)
-#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    profile_photo_id = db.Column(db.Integer, db.ForeignKey('Profile_Photo.id'))
+    profile_photo = db.relationship('Profile_Photo', backref='user', uselist=False)
 
-    # ADD TO USER TABLE!!
-    # profile_photo_id = db.Column(db.Integer, db.ForeignKey('Profile_Photo.id'))
-    # profile_photo = db.relationship('Profile_Photo', backref='user', uselist=False)
+    saved_artist = db.relationship('Saved_Artist', backref='user')
+    saved_music = db.relationship('Saved_Music', backref='user')
 
-    # saved_artist = db.relationship('Saved_Artist', backref='user')
-    # saved_music = db.relationship('Saved_Music', backref='user')
+    followed_artists = db.relationship('Follow_Artist', backref='user')
 
+    def __repr__(self):
+        return f'<User {self.email}>'
 
-#     def __repr__(self):
-#         return f'<User {self.email}>'
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "email": self.email,
-#             "saved_artist": [saved.serialize() for saved in self.saved_artist],
-#             "saved_music": [saved.serialize() for saved in self.saved_music],
-                   # do not serialize the password, its a security breach
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "saved_artist": [saved.serialize() for saved in self.saved_artist],
+            "saved_music": [saved.serialize() for saved in self.saved_music],
+        }
     
-# class Artist(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     password = db.Column(db.String(80), unique=False, nullable=False)
-#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+    profile_photo_id = db.Column(db.Integer, db.ForeignKey('Profile_Photo.id'))
+    profile_photo = db.relationship('Profile_Photo', backref='artist', uselist=False)
 
-    # ADD TO ARTIST TABLE!!
-    # profile_photo_id = db.Column(db.Integer, db.ForeignKey('Profile_Photo.id'))
-    # profile_photo = db.relationship('Profile_Photo', backref='artist', uselist=False)
-    # artist_photos = db.relationship("Photo", backref="artist")
-    # artist_videos = db.relationship("Video", backref="artist")
-    # artist_music = db.relationship("Music", backref="artist")
-    # saved_artist = db.relationship('Saved_Artist', backref='artist')
+    artist_bio = db.relationship("Photo", backref="artist")
+    artist_photos = db.relationship("Photo", backref="artist")
+    artist_videos = db.relationship("Video", backref="artist")
+    artist_music = db.relationship("Music", backref="artist")
+
+    saved_artist = db.relationship('Saved_Artist', backref='artist')
+
+    followers = db.relationship('Follow_Artist', backref='artist')
+
+    def __repr__(self):
+        return f'<Artist {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+        }
     
-
-#     def __repr__(self):
-#         return f'<Artist {self.email}>'
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "email": self.email,
-#             # do not serialize the password, its a security breach
-#         }
-    
-    #  PROFILE PHOTO CLASS MODEL
+                # USER & ARTIST PROFILE PHOTO MODEL
 class Profile_Photo(db.Model):
     __tablename__ = "profile_photo"
     
     id = db.Column(db.Integer, primary_key=True)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    user_profile_photo = db.Column(db.String(255), nullable=True, unique=True)  # img URL or file path
-    artist_profile_photo = db.Column(db.String(255), nullable=True, unique=True)  # img URL or file path
-
-    entity_type = db.Column(db.String(50), nullable=False)  # 'user' or 'artist'
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), unique=True, nullable=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey("artist.id"), unique=True, nullable=True)
 
     def __repr__(self):
-        return f'<ProfilePhoto {self.id, self.user_profile_photo, self.artist_profile_photo}>'
+        return f"<ProfilePhoto id={self.id}, user_id={self.user_id}, artist_id={self.artist_id}, profile_photo={self.profile_photo}>"
 
     def serialize(self):
-        data = {
-        "id": self.id,
-        "is_active": self.is_active,
+        return {
+            "id": self.id,
+            "is_active": self.is_active,
+            "profile_photo": self.profile_photo,
+            "user_id": self.user_id,
+            "artist_id": self.artist_id
         }
-        if self.user_profile_photo:
-            data["user_profile_photo"] = self.user_profile_photo
-        if self.artist_profile_photo:
-            data["artist_profile_photo"] = self.artist_profile_photo
-        return data
 
-    # ARTIST POSTS CLASS
+                # ARTIST BIO MODEL
+class Bio(db.Model):
+    __tablename__ = "bio"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    text = db.Column(db.String(1000), nullable=True)
+
+    artist_id = db.Column(db.Integer, db.ForeignKey("artist.id"), nullable=False)
+
+    def __repr__(self):
+        shortened_text = self.text[:50] + '...' if self.text else "No bio available"
+        return f'<Bio text={shortened_text}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "artist_id": self.artist_id
+        }
+
+
+                # ARTIST PHOTO MODEL
 class Photo(db.Model):
     __tablename__ = "photo"
 
@@ -101,14 +118,14 @@ class Photo(db.Model):
             "media_url": self.media_url
         }
 
-
+                # ARTIST VIDEO MODEL
 class Video(db.Model):
     __tablename__ = "video"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     media_url = db.Column(db.Text, nullable=False, unique=True)  # Cloudinary URL
-    duration = db.Column(db.Integer, nullable=False)  # Duration in seconds
+    duration = db.Column(db.Integer, nullable=False)
 
     artist_id = db.Column(db.Integer, db.ForeignKey("artist.id"), nullable=False)
 
@@ -123,13 +140,14 @@ class Video(db.Model):
             "duration": self.duration,
         }
 
+                # ARTIST MUSIC MODEL
 class Music(db.Model):
     __tablename__ = "music"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     media_url = db.Column(db.Text, nullable=False, unique=True)  # Cloudinary URL
-    duration = db.Column(db.Integer, nullable=False)  # Duration in seconds
+    duration = db.Column(db.Integer, nullable=False)
 
     artist_id = db.Column(db.Integer, db.ForeignKey("artist.id"), nullable=False)
 
@@ -144,8 +162,8 @@ class Music(db.Model):
             "duration": self.duration,
         }
 
-    # USER SAVED MUSIC / ARTISTS
 
+                # USER SAVED MUSIC & SAVED ARTISTS MODEL
 class Saved_Artist(db.Model):
     __tablename__ = "saved_artist"
 
@@ -171,9 +189,7 @@ class Saved_Music(db.Model):
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     artist_id = db.Column(db.Integer, db.ForeignKey("artist.id"))
-    
     music_id = db.Column(db.Integer, db.ForeignKey("music.id"))
-
 
     def __repr__(self):
         return f'<Saved_Music {self.music_id}>'
@@ -184,4 +200,24 @@ class Saved_Music(db.Model):
             "user_id": self.user_id,
             "artist_id": self.artist_id,
             "music_id": self.music_id
+        }
+    
+
+                # FOLLOW ARTIST MODEL
+class Follow_Artist(db.Model):
+    __tablename__ = "follow_artist"
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey("artist.id"), primary_key=True)
+
+    is_active = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return f'<Follow_Artist user_id={self.user_id}, artist_id={self.artist_id}, is_active={self.is_active}>'
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "artist_id": self.artist_id,
+            "is_active": self.is_active
         }
